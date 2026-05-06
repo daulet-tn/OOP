91fa920874e0194b2ce6dbb41189fb40dae7e4b2
daulet_tn <daulettanirberdi94@gmail.com>
2026-05-06 16:50:31 +0500

    Update User.java

---
+++ users/User.java
@@ -0,0 +1,34 @@
+import enums.UserRole;
+
+public abstract class User{
+    protected String userId;
+    protected String firstName;
+    protected String lastName;
+    protected String email;
+    protected UserRole role;
+
+    public User(String userId, String firstName, String lastName, String email, UserRole role){
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.role = role;
+    }
+
+    // Getters
+    public String getUserId(){return userId;}
+    public String getFirstName(){return firstName;}
+    public String getLastName(){return lastName;}
+    public String getEmail(){return email;}
+    public String getRole(){return role;}
+
+    //Setters 
+    public void setFirstName(String firstName){this.firstName = firstName;}
+    public void setLastName(String lastName){this.lastName = lastName;}
+    public void setEmail(String email){this.email = email;}
+
+    @Override
+    punlic String toString(){
+        return firstName + " " + lastName + " ( " + role + " )";
+    }
+}

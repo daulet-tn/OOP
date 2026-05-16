bcb8cba0ff6d879e042a5ae10d57848642f56d6d
daulet_tn <daulettanirberdi94@gmail.com>
2026-05-16 22:37:31 +0500

    Update Admin.java

---
+++ users/Admin.java
@@ -0,0 +1,8 @@
+import enums.UserRole;
+
+public class Admin extends User{
+
+    public Admin(String userId, String firstName, String lastName, String email, String passwordHash){
+        super(userId)
+    }
+}

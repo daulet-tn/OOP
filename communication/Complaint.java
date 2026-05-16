53c0464f22bc6c3d83fd23296d8f5f8c6b560196
daulet_tn <daulettanirberdi94@gmail.com>
2026-05-17 02:41:41 +0500

    Complaint

---
+++ communication/Complaint.java
@@ -0,0 +1,44 @@
+import enums.RequestStatus;
+import users.User;
+import interfaces.Printable;
+
+public class Complaint implements Printable{
+    private static int counter = 0;
+    private String complaintId;
+    private User sender;
+    private String subject;
+    private String despcription;
+    private RequestStatus status;
+
+    public Complaint(User sender, String subject, String despcription){
+        this.complaintId = "CMP- " + (++counter);
+        this.sender = sender;
+        this.subject = subject;
+        this.despcription = despcription;
+        this.status = RequestStatus.PENDING;
+    }
+
+    public void resolve(){
+        this.status = RequestStatus.APPROVED;
+        System.out.println("Complaint " + complaintId + " resolved.");
+    }
+
+    //Getters
+    public String getComplaintId() { return complaintId; }
+    public User getSender() { return sender; }
+    public String getSubject() { return subject; }
+    public String getDescription() { return description; }
+    public RequestStatus getStatus() { return status; }
+
+
+    @Override
+    public String print(){
+        return return "Complaint ( id = " + complaintId + ", subject = "
+        + subject + ", status = " + status + " )";
+    }
+
+    @Override
+    public String toString(){
+        return "(" + status + " )" + subject + ": " + description;
+    }
+}
